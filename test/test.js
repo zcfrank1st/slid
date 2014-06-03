31b3e41f6a01bc945370dbffb6f2fa1777e56d6c
zcfrank1st <zhchaos@gmail.com>
2014-06-03 14:33:34 +0800

    修改测试

--- test/test.js
+++ test/test.js
@@ -3,22 +3,23 @@ var fs = require('fs');
 var slid = require('../index');
 
 describe('slid template generate config test', function () {
-  it('generate a config file', function () {
-    slid.generateConfigFile(3);
-    fs.existsSync(process.cwd() + '/slides.yml').should.be.ok;
-  });
+    it('generate a config file', function () {
+        slid.generateConfigFile(3);
+        fs.existsSync(process.cwd() + '/slides.yml').should.be.ok;
+    });
 });
 
 describe('slid template render', function () {
-  it('render the template and generate a rendered file', function () {
-    slid.renderTemplate(3, function(){
-      fs.existsSync(__dirname + '/target/slide.html').should.be.ok;
+    it('render the template and generate a rendered file', function () {
+        slid.renderTemplate(3);
+        setTimeout(function () {
+            fs.existsSync(__dirname + '/target/slide.html').should.be.ok;
+        }, 5000);
     });
-  });
 });
 
 describe('slid open slides', function () {
-  it('must open a browser contains slides', function () {
-    slid.start();
-  });
+    it('must open a browser contains slides', function () {
+        slid.start();
+    });
 });
